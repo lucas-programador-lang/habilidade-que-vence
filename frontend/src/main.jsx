7a4b2ae0b2287b2320fbbadddd21d1e8f82ae8b9
lucas-programador-lang <lucas.dev.programador@outlook.com>
2026-07-25 17:49:45 -0400

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div>
+      <h1>Habilidade que Vence</h1>
+      <p>Site no ar com sucesso!</p>
+    </div>
+  </React.StrictMode>,
+)
